9b7048e2268f1ef206b4b094fbca921c054b5be6
JurasLeo <165816405+JurasLeo@users.noreply.github.com>
2024-04-09 14:59:14 +0200

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector('.toggle-bar')
-const toggleBar = document.querySelector('.nav-bar')
-nav.addEventListener('click',function(){
-    nav.classList.toggle("active");
-    toggleBar.classList.toggle("active");
-    
-});
